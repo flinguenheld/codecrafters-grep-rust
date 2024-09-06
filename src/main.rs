f35e6b87ffab4edc36a7d47b85e485e4ee717b2c
flo <f@linguenheld.fr>
2024-09-06 11:03:01 +0200

    Digit match

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,60 @@ use std::env;
 use std::io;
 use std::process;
 
-fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    if pattern.chars().count() == 1 {
-        return input_line.contains(pattern);
-    } else {
-        panic!("Unhandled pattern: {}", pattern)
-    }
-}
-
 // Usage: echo <input_text> | your_program.sh -E <pattern>
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    // println!("Logs from your program will appear here!");
 
-    if env::args().nth(1).unwrap() != "-E" {
-        println!("Expected first argument to be '-E'");
-        process::exit(1);
-    }
-
-    let pattern = env::args().nth(2).unwrap();
     let mut input_line = String::new();
+    if io::stdin().read_line(&mut input_line).is_ok() {
+        let mut found = false;
+        if let Some(pattern) = env::args().last() {
+            for arg in env::args() {
+                match arg.as_str() {
+                    "-E" => {
+                        if input_line.contains(pattern.as_str()) {
+                            found = true;
+                            break;
+                        }
+                    }
+                    "\\d" => {
+                        if input_line.chars().any(|c| c.is_digit(10)) {
+                            found = true;
+                            break;
+                        }
+                    }
+                    _ => {}
+                }
+            }
 
-    io::stdin().read_line(&mut input_line).unwrap();
+            match found {
+                true => {
+                    println!("Found");
+                    process::exit(0)
+                }
+                false => {
+                    println!("Not found");
+                    process::exit(1)
+                }
+            }
 
-    // Uncomment this block to pass the first stage
-    if match_pattern(&input_line, &pattern) {
-        process::exit(0)
-    } else {
-        process::exit(1)
+            // if env::args().nth(1).unwrap() != "-E" {
+            //     println!("Expected first argument to be '-E'");
+            //     process::exit(1);
+            // }
+
+            // let pattern = env::args().nth(2).unwrap();
+            // let mut input_line = String::new();
+
+            // Uncomment this block to pass the first stage
+            // if match_pattern(&input_line, &pattern) {
+            //     println!("Pattern '{}' found", pattern);
+            //     process::exit(0)
+            // } else {
+            //     process::exit(1)
+            // }
+        }
     }
+    process::exit(1)
 }
