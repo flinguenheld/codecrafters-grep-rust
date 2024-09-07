f5391ec3134bf0db9c59d6425c0340b6949fecd8
flo <f@linguenheld.fr>
2024-09-08 00:16:36 +0200

    Remove reverse func

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::io;
 use std::process;
+use std::rc::Rc;
 
 // Usage: echo <input_text> | your_program.sh -E <pattern>
 fn main() {
@@ -9,7 +10,7 @@ fn main() {
 
     let mut input_line = String::new();
     if io::stdin().read_line(&mut input_line).is_ok() {
-        let mut pat: Vec<Box<dyn Fn(char) -> bool>> = Vec::new();
+        let mut pat: Vec<Rc<dyn Fn(char) -> bool>> = Vec::new();
         if let Some(mut raw_pattern) = env::args().last() {
             let mut start_end = (false, false);
             if raw_pattern.starts_with('^') {
@@ -30,10 +31,10 @@ fn main() {
                 } else if current.starts_with('\\') {
                     match c {
                         'd' => {
-                            pat.push(Box::new(|c: char| c.is_ascii_digit()));
+                            pat.push(Rc::new(|c: char| c.is_ascii_digit()));
                         }
                         'w' => {
-                            pat.push(Box::new(|c: char| c.is_ascii_alphanumeric()));
+                            pat.push(Rc::new(|c: char| c.is_ascii_alphanumeric()));
                         }
                         _ => {}
                     }
@@ -53,26 +54,34 @@ fn main() {
 
                     // Farfetch -_-'
 
-                    pat.push(Box::new(move |ch: char| ascii_reverse.contains(ch)));
+                    pat.push(Rc::new(move |ch: char| ascii_reverse.contains(ch)));
                 } else if current.starts_with('[') && current.ends_with(']') {
                     let blah: String = current
                         .chars()
                         .filter(|cc| cc.is_ascii_alphanumeric())
                         .collect();
-                    pat.push(Box::new(move |ch: char| blah.contains(ch)));
+                    pat.push(Rc::new(move |ch: char| blah.contains(ch)));
                 } else {
                     println!("Add just a char: {}", c);
-                    pat.push(Box::new(move |ch: char| ch == c));
+                    pat.push(Rc::new(move |ch: char| ch == c));
                     current.clear();
                 }
             }
 
             let found = match start_end {
                 (true, false) => test_pattern(&input_line, &pat, true),
-                (false, true) => test_pattern_reverse(&input_line, &pat, true),
+                (false, true) => test_pattern(
+                    &input_line.chars().rev().collect(),
+                    &pat.iter().rev().cloned().collect(),
+                    true,
+                ),
                 (true, true) => {
                     test_pattern(&input_line, &pat, true)
-                        && test_pattern_reverse(&input_line, &pat, true)
+                        && test_pattern(
+                            &input_line.chars().rev().collect(),
+                            &pat.iter().rev().cloned().collect(),
+                            true,
+                        )
                 }
                 _ => test_pattern(&input_line, &pat, false),
             };
@@ -95,7 +104,7 @@ fn main() {
 
 fn test_pattern(
     input_line: &String,
-    pattern: &Vec<Box<dyn Fn(char) -> bool>>,
+    pattern: &Vec<Rc<dyn Fn(char) -> bool>>,
     on_start_only: bool,
 ) -> bool {
     'aaa: for i in 0..input_line.chars().count() {
@@ -107,37 +116,6 @@ fn test_pattern(
                 if let Some(c) = inp_iter.next() {
                     if !(p)(c) {
                         if on_start_only {
-                            return false;
-                        } else {
-                            continue 'aaa;
-                        }
-                    }
-                } else {
-                    continue 'aaa;
-                }
-            } else {
-                return true;
-            }
-        }
-    }
-
-    false
-}
-fn test_pattern_reverse(
-    input_line: &String,
-    pattern: &Vec<Box<dyn Fn(char) -> bool>>,
-    // Remove that ?
-    on_end_only: bool,
-) -> bool {
-    'aaa: for i in 0..input_line.chars().count() {
-        let mut pat_iter = pattern.iter().rev();
-        let mut inp_iter = input_line.chars().rev().skip(i);
-
-        loop {
-            if let Some(p) = pat_iter.next() {
-                if let Some(c) = inp_iter.next() {
-                    if !(p)(c) {
-                        if on_end_only {
                             return false;
                         } else {
                             continue 'aaa;
